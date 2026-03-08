ed338807deb3c0f66d4429c59a7b9e2013f950dc
Ly1644 <3397618603@qq.com>
2026-03-08 23:35:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-
 from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
@@ -16,4 +16,5 @@ def index():
     return jsonify({'hello': 'world'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
